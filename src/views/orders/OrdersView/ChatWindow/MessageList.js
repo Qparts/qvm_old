6131d7a2ed4cec6fbcbf46665c1dd198f24097d5
Rabea Saad <rabiaasaad44@gmail.com>
2021-08-30 10:49:17 +0200

    kkk

--- src/views/orders/OrdersView/ChatWindow/MessageList.js
+++ src/views/orders/OrdersView/ChatWindow/MessageList.js
@@ -1,67 +1,101 @@
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import MessageItem from './MessageItem';
-import React, { useEffect, useRef } from 'react';
-import Scrollbars from 'src/components/Scrollbars';
+import Page from 'src/components/Page';
+import React, { useEffect } from 'react';
+import { useHistory } from "react-router";
 import { makeStyles } from '@material-ui/core/styles';
-import { useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Box } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+import LoadingScreen from 'src/components/LoadingScreen';
+import LoadingOverlay from "react-loading-overlay";
+import AvailabilityPartsSection from './AvailabilityPartsSection';
+import ProductInfoSection from './ProductInfoSection';
+import { cleanup } from 'src/redux/slices/partSearch';
+import { PATH_APP } from 'src/routes/paths';
+import EmptyContent from "../../../components/Ui/EmptyContent";
+import SearchBar from "../../../components/Ui/SearchBar";
 
 // ----------------------------------------------------------------------
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
-  scroll: { height: '100%', padding: theme.spacing(3) }
+    root: {
+        boxShadow: 'none',
+        textAlign: 'center',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+            textAlign: 'left',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+        },
+        [theme.breakpoints.up('xl')]: {
+            height: 320
+        }
+    }
 }));
 
 // ----------------------------------------------------------------------
 
-MessageList.propTypes = {
-  // conversation: PropTypes.object.isRequired,
-  className: PropTypes.string
-};
+function PartSearchView() {
+    const classes = useStyles();
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { isLoading, productInfoResult = [],
+        productResult = [], error } = useSelector((state) => state.PartSearch);
 
-function MessageList({ className, ...other }) {
-  const classes = useStyles();
-  const scrollRef = useRef();
-  const { pathname } = useLocation();
-  const {
-    messages,
-    activeConversation,
-  } = useSelector((state) => state.chat);
+    useEffect(() => {
+        return () => {
+            dispatch(cleanup())
+        }
+    }, []);
 
-  const { user } = useSelector((state) => state.authJwt);
+    return (
+        <Page
+            title={t("Search Parts")}
+            className={classes.root}>
 
-  //members of active conversation.
-  const currentContact = activeConversation?.members?.filter(x => x.id != user.subscriber.id);
+            <LoadingOverlay
+                active={isLoading}
+                styles={{
+                    wrapper: {
+                        width: "100%",
+                        height: "100%",
+                    },
+                }}
+                spinner={
+                    <LoadingScreen />
 
+                }>
 
+                {error != null && error === "Search limit exceeded!" ?
+                    <EmptyContent
+                        btnTitle={t("Upgrade to Premium")}
+                        title={t("Search limit exceeded")}
+                        description={t("Search limit exceeded!")}
+                        url={() => history.push(PATH_APP.general.upgradeSubscription)}
+                    />
+                    :
+                    productInfoResult.length === 0 && productResult.length === 0 && isLoading === false ?
+                        <EmptyContent
+                            btnHome
+                            title={t("Unable to receive your order")}
+                            description={t("look like there are no results for the item you were looking for")}
+                        />
+                        :
+                        <>
+                            {productResult.length > 0 && <SearchBar searchPartStyl='searchPartStyl' />}
+                            {productResult.length > 0 ? <AvailabilityPartsSection /> : null}
+                            {productInfoResult.length > 0 ?
+                                <>
+                                    <Box sx={{ mb: 6 }} />
+                                    <ProductInfoSection />
+                                </> : null
+                            }
+                        </>
+                }
+            </LoadingOverlay>
 
-  useEffect(() => {
-    const scrollMessagesToBottom = () => {
-      if (scrollRef.current) {
-        scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-      }
-    };
-    scrollMessagesToBottom();
-  }, [messages , activeConversation]);
-
-  return (
-    <Scrollbars
-      scrollableNodeProps={{ ref: scrollRef }}
-      className={classes.scroll}
-    >
-      <div className={clsx(classes.root, className)} {...other}>
-        { pathname != '/app/chat/new' && messages?.map((message , index) => (
-          <MessageItem
-            key={index}
-            message={message}
-            currentContact={currentContact}
-          />
-        ))}
-      </div>
-    </Scrollbars>
-  );
+        </Page>
+    );
 }
 
-export default MessageList;
+export default PartSearchView;
